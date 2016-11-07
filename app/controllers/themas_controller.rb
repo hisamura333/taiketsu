0c7f54efe067ec488cec9759c62aabb238fe71c8
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-11-07 14:36:23 +0900

    Add create action

--- app/controllers/themas_controller.rb
+++ app/controllers/themas_controller.rb
@@ -4,6 +4,14 @@ class ThemasController < ApplicationController
   end
   def show
     @thema = Thema.new
-    
+  end
+  def create
+    @thema = Thema.create(create_params)
+    redirect_to root_path
+  end
+
+  private
+  def create_params
+    params.require(:thema).permit(:first_name,:second_name)
   end
 end
