6b89598ae64dbcadeda8ecf6b8aa4277797edb66
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-11-07 20:40:12 +0900

    Nest ‘theme’ ‘reviews’

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "themas#index"
-  resources :themas
-  resources :reviews
+  resources :themas do
+    resources :reviews
+  end
 end
