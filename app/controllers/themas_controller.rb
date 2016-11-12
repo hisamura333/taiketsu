39c736a937aa34919c092981f43e9ba6f70f29e5
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-11-12 17:47:38 +0900

    Create next_link previous_link

--- app/controllers/themas_controller.rb
+++ app/controllers/themas_controller.rb
@@ -16,6 +16,17 @@ class ThemasController < ApplicationController
     @new_thema = Thema.new
     @thema = Thema.find(params[:id])
     @review = Review.new
+
+    thema_ids = Review.group(:thema_id).order('count_thema_id DESC').count(:thema_id).keys
+
+    @ranking = thema_ids.map { |id| Thema.find(id)}
+    @themas = Thema.all
+
+    @themas.each do |thema|
+      if thema.reviews.blank?
+        @ranking << thema
+      end
+    end
   end
   def create
     @thema = Thema.create(thema_params)
