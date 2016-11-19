b2338d2128d426d7424b9e46bc393d9042269a05
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-11-19 16:23:58 +0900

    Unnecessary file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107104457) do
+ActiveRecord::Schema.define(version: 20161116065033) do
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "first_body",  limit: 65535
@@ -18,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161107104457) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "thema_id"
+    t.integer  "likes_count"
   end
 
   create_table "themas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -25,6 +32,23 @@ ActiveRecord::Schema.define(version: 20161107104457) do
     t.datetime "updated_at",  null: false
     t.string   "first_name"
     t.string   "second_name"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
