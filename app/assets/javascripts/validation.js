613b5b50303b4e7c04addde4f6e99696edb72322
hisamura333 <HisamuraTaiki@hisamura-no-MacBook-Air.local>
2016-11-29 19:36:21 +0900

    Create validation create them

---
+++ app/assets/javascripts/validation.js
@@ -0,0 +1,16 @@
+$(function(){
+  if ($('.first_post').val().length ==0 || $('.second_post').val().length ==0){
+    $('.submit__form').attr('disabled', 'disabled');
+  }
+  $('.first_post,.second_post').bind('keydown keyup keypress change', function() {
+    if ($('.first_post').val().length > 0 && $('.second_post').val().length >0) {
+      $('.submit__form').removeAttr('disabled');
+      $('.submit__form').css('background','#81b3ab');
+    } else {
+      $('.submit__form').attr('disabled', 'disabled');
+      $('.submit__form').css('background','#4e8078');
+    }
+    console.log('ccccc')
+  });
+});
+
